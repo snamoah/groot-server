1c67a47055ffac3efc76c74ae5f7c3b88ab4d594
Samuel Amoah <snamoah@gmail.com>
2017-05-15 15:27:57 +0000

    add allow origin header to CORS

--- dist/index.js
+++ dist/index.js
@@ -29,7 +29,7 @@ var server = _restify2.default.createServer({
 // use default accept parser
 server.use(_restify2.default.CORS({
   'origins': ['*'],
-  'headers': ['Access-Control-Origin-Request-Method', 'Access-Control-Request-Headers']
+  'headers': ['Access-Control-Origin-Request-Method', 'Access-Control-Request-Headers', 'Access-Control-Allow-Origin']
 }));
 
 server.use(_restify2.default.bodyParser());
